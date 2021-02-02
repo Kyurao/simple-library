7cc6787ef6559a0aca69953a92ae2876c01abacf
kyurao <yuriikutselia@gmail.com>
2021-02-03 00:32:25 +0200

    Add RequestMapping to controllers.

--- src/main/java/com/kyurao/simplelibray/controller/BookController.java
+++ src/main/java/com/kyurao/simplelibray/controller/BookController.java
@@ -1,7 +1,9 @@
 package com.kyurao.simplelibray.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/book")
+@RestController
+@RequestMapping("/book")
 public class BookController {
 }
--- src/main/java/com/kyurao/simplelibray/controller/UserController.java
+++ src/main/java/com/kyurao/simplelibray/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.kyurao.simplelibray.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/user")
+@RestController
+@RequestMapping("/user")
 public class UserController {
 }
