d75931a14efa76bfed4d1d81ef269b97311f8df2
kyurao <yuriikutselia@gmail.com>
2021-02-02 17:55:58 +0200

    Add User and Book controllers, services, repositories.

---
+++ src/main/java/com/kyurao/simplelibray/controller/BookController.java
@@ -0,0 +1,7 @@
+package com.kyurao.simplelibray.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/book")
+public class BookController {
+}
---
+++ src/main/java/com/kyurao/simplelibray/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.kyurao.simplelibray.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/user")
+public class UserController {
+}
