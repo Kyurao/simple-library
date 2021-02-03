f987fcd2bbb959679f706bfb4b4ce40c74b95b81
kyurao <yuriikutselia@gmail.com>
2021-02-03 02:03:25 +0200

    Add Book and User endpoints.

--- src/main/java/com/kyurao/simplelibray/controller/BookController.java
+++ src/main/java/com/kyurao/simplelibray/controller/BookController.java
@@ -1,9 +1,43 @@
 package com.kyurao.simplelibray.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.kyurao.simplelibray.dto.request.BookReq;
+import com.kyurao.simplelibray.dto.response.BookRes;
+import com.kyurao.simplelibray.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/book")
+@RequiredArgsConstructor
 public class BookController {
+
+    private final BookService bookService;
+
+    @GetMapping("{id}")
+    public BookRes getBook(@PathVariable Long id) {
+        return bookService.getBook(id);
+    }
+
+    @GetMapping("all")
+    public List<BookRes> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @PostMapping("create")
+    public void createNewBook(@RequestBody BookReq req) {
+        bookService.addNewBook(req);
+    }
+
+    @PutMapping("update/{id}")
+    public void updateBook(@PathVariable Long id,
+                           @RequestBody BookReq req) {
+        bookService.editBook(id, req);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+    }
 }
--- src/main/java/com/kyurao/simplelibray/controller/UserController.java
+++ src/main/java/com/kyurao/simplelibray/controller/UserController.java
@@ -1,9 +1,49 @@
 package com.kyurao.simplelibray.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.kyurao.simplelibray.dto.request.NewUserReq;
+import com.kyurao.simplelibray.dto.request.UserReq;
+import com.kyurao.simplelibray.dto.response.UserRes;
+import com.kyurao.simplelibray.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("user")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("{id}") //localhost:8800/user/1
+    public UserRes getUser(@PathVariable("id") Long userId) {
+        return userService.getUser(userId);
+    }
+
+    @GetMapping //localhost:8800/user?id=1
+    public UserRes getUserByParam(@RequestParam Long id) {
+        return userService.getUser(id);
+    }
+
+    @GetMapping("all")
+    public List<UserRes> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("create")
+    public void createNewUser(@RequestBody NewUserReq req) {
+        userService.addNewUser(req);
+    }
+
+    @PutMapping("update/{id}")
+    public void updateUser(@PathVariable Long id,
+                           @RequestBody UserReq req) {
+        userService.editUser(id, req);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
 }
